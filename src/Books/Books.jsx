91f5d7d7700a1e0e49d796e1c1b9fff42d42d1da
Muhammad <mallllllaev@gmail.com>
2021-12-03 13:07:13 +0300

    added waiting photo

--- src/Books/Books.jsx
+++ src/Books/Books.jsx
@@ -4,33 +4,40 @@ import Book from "./Book/Book";
 import Search from "./Search/Search";
 import MoreBooks from "./MoreBooks/MoreBooks";
 import Preloader from "../assets/Preloader/Preloader";
+import books from './../assets/books.png'
 
 const Books = (props) => {
 
-    let booksElements = props.books.map((b)=>{
-        return <Book key = {b.id} title = {b.volumeInfo.title}
-                     authors ={b.volumeInfo.authors} id = {b.id}
-            subject={b.volumeInfo.categories===undefined ? 'not found': b.volumeInfo.categories[0]}
-                     photo = {b.volumeInfo.imageLinks===undefined ? null : b.volumeInfo.imageLinks.thumbnail}/>
+    let booksElements = props.books.map((b) => {
+        return <Book key={b.id} title={b.volumeInfo.title}
+                     authors={b.volumeInfo.authors} id={b.id}
+                     subject={b.volumeInfo.categories === undefined ? 'not found' : b.volumeInfo.categories[0]}
+                     photo={b.volumeInfo.imageLinks === undefined ? null : b.volumeInfo.imageLinks.thumbnail}/>
     })
 
     return (
         <div className={c.booksPage}>
-                <Search newSearchText = {props.newSearchText} resultsCount = {props.resultsCount}
-                        pressEnter = {props.pressEnter} changeSearchText = {props.changeSearchText}
-                        selectCategory = {props.selectCategory} selectSortBy = {props.selectSortBy}
-                        onButton = {props.onButton} isFetching = {props.isFetching}
-                />
+            <Search newSearchText={props.newSearchText} resultsCount={props.resultsCount}
+                    pressEnter={props.pressEnter} changeSearchText={props.changeSearchText}
+                    selectCategory={props.selectCategory} selectSortBy={props.selectSortBy}
+                    onButton={props.onButton} isFetching={props.isFetching}
+            />
 
-                <div className={c.books}>{booksElements}</div>
 
-                <div>
-                    { (props.isFetching && props.resultsCount !==null) ?
-                        <div className={c.preloader}><Preloader/></div> :
-                        (props.resultsCount < 30 || props.resultsCount === 0 ||
-                            props.resultsCount-props.startIndex<30) ?
-                            null : <MoreBooks onButton = {props.onMoreBooksButton}/>}
-                </div>
+            {props.resultsCount === null ?
+                <div className={c.booksPngDiv}>
+                    <img src={books} className={c.booksPng} alt=""/>
+                    <div className={c.booksPngText}>waiting to search!</div>
+                </div> : null}
+            <div className={c.books}>{booksElements}</div>
+
+            <div>
+                {(props.isFetching && props.resultsCount !== null) ?
+                    <div className={c.preloader}><Preloader/></div> :
+                    (props.resultsCount < 30 || props.resultsCount === 0 ||
+                        props.resultsCount - props.startIndex < 30) ?
+                        null : <MoreBooks onButton={props.onMoreBooksButton}/>}
+            </div>
         </div>
     )
 }
